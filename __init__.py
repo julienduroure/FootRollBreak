46b224f0f09730101526af8fbe694de025eea2da
Julien Duroure <julien.duroure@gmail.com>
2016-03-10 16:18:31 +0100

    bump to 0.9.0 --> Seems we have a full working version now. Need to be tested :)

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
 	"name": "Add FootRoll Break to Rigify",
 	"author": "Julien Duroure",
-	"version": (0, 0, 3),
+	"version": (0, 9, 0),
 	"blender": (2,77, 0),
 	"description": "Add FootRoll Break to Rigify",
 	"location": "Armature Properties, View 3D Properties",
