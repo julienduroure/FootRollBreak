cbf691711df9559eff79b513f09ffa2ca97fd509
Julien Duroure <julien.duroure@gmail.com>
2016-03-15 22:10:47 +0100

    bump to v1.0.0

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@
 bl_info = {
 	"name": "Add FootRoll Break to Rigify",
 	"author": "Julien Duroure",
-	"version": (0, 9, 0),
+	"version": (1, 0, 0),
 	"blender": (2,77, 0),
 	"description": "Add FootRoll Break to Rigify",
 	"location": "Armature Properties, View 3D Properties",
